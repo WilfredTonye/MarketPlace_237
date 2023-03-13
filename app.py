49ab00e1aa2b6fa422c7336661be86594c9fc7a4
DISSANG <dissangfrancis@yahoo.com>
2023-03-14 00:19:48 +0100

    AUTHENTIFICATION ADDED IN APPLICATION

--- app.py
+++ app.py
@@ -1,16 +1,84 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_moment import Moment
+from functools import wraps
+from authlib.integrations.flask_client import OAuth
 import os
 from dotenv import load_dotenv
-
-load_dotenv() # charge le fichier .env dans les variables d'environnement
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL') # récupère l'URL de connexion à partir des variables d'environnement
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # désactive le suivi des modifications pour améliorer les performances
-db = SQLAlchemy(app)
-
 from models import*
+# charge le fichier .env dans les variables d'environnement
+load_dotenv() 
+app = Flask(__name__)
+moment = Moment(app)
+
+#Gestion de l'authentification grace à Auth0
+app.config['SECRET_KEY'] = os.urandom(32)
+app.config['AUTH0_CLIENT_ID'] = os.getenv('AUTH0_CLIENT_ID')
+app.config['AUTH0_CLIENT_SECRET'] = os.getenv('AUTH0_CLIENT_SECRET')
+app.config['AUTH0_DOMAIN'] = os.getenv('AUTH0_DOMAIN')
+app.config['AUTH0_CALLBACK_URL'] = os.getenv('AUTH0_CALLBACK_URL')
+app.config['AUTH0_AUDIENCE'] = os.getenv('AUTH0_AUDIENCE')
+db = db_setup(app)
+# db.create_all()
+oauth = OAuth(app)
+
+auth0 = oauth.register(
+    'auth0',
+    client_id=app.config['AUTH0_CLIENT_ID'],
+    client_secret=app.config['AUTH0_CLIENT_SECRET'],
+    api_base_url=f'https://{app.config["AUTH0_DOMAIN"]}',
+    access_token_url=f'https://{app.config["AUTH0_DOMAIN"]}/oauth/token',
+    authorize_url=f'https://{app.config["AUTH0_DOMAIN"]}/authorize',
+    client_kwargs={
+        'scope': 'openid profile email',
+    },
+)
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth0_url = f'https://{app.config["AUTH0_DOMAIN"]}/authorize'
+        token = None
+
+        # Check for access token in request headers
+        if 'Authorization' in request.headers:
+            token = request.headers['Authorization'].split(' ')[1]
+        else:
+            return jsonify({'message': 'No authorization header provided.'}), 401
+
+        # Validate access token
+        try:
+            payload = auth0.api_base.get('userinfo', token=token).json()
+        except:
+            return jsonify({'message': 'Invalid authorization token.'}), 401
+
+        # Check for valid user role
+        if 'role' in payload and payload['role'] == 'seller':
+            return f(*args, **kwargs)
+        else:
+            return jsonify({'message': 'Insufficient permissions.'}), 403
+
+    return decorated
+
+@app.route('/login')
+def login():
+    return auth0.authorize_redirect(redirect_uri=app.config['AUTH0_CALLBACK_URL'])
+
+@app.route('/callback')
+def callback_handling():
+    auth0.authorize_access_token()
+    # resp = auth0.get('userinfo')
+    # userinfo = resp.json()
+    print("utilisateur authentifié Avec success")
+    # return jsonify(userinfo)
+
+@app.route('/protected')
+@requires_auth
+def protected_route():
+    return jsonify({'message': 'You are authorized to access this resource.'})
+
+
 
 @app.route('/', methods=['GET'])
 def home():
--- models.py
+++ models.py
@@ -1,22 +1,50 @@
-from app import db
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+import os
+from flask_migrate import Migrate
+db = SQLAlchemy()
 
+
+
+# TODO: connect to a local postgresql database
+def db_setup(app):
+   #Connexion à la base de donnees
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL') # récupère l'URL de connexion à partir des variables d'environnement
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # désactive le suivi des modifications pour améliorer les performances
+    db.app = app
+    db.init_app(app)
+    migrate = Migrate(app, db)
+    return db
 #ZONE POUR LE MODELE DE DONNEE
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    role = db.Column(db.String(10), nullable=False)
+    subscription = db.relationship('Subscription', backref='user', uselist=False)
+    products = db.relationship('Product', backref='user', lazy=True)
 
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
+class Subscription(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    start_date = db.Column(db.DateTime, default=datetime.utcnow)
+    end_date = db.Column(db.DateTime)
 
-#     def __repr__(self):
-#         return '<User %r>' % self.username
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    buyers = db.relationship('Buyer', secondary='orders', backref='products')
 
-# class Post(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     title = db.Column(db.String(120), nullable=False)
-#     content = db.Column(db.Text, nullable=False)
-#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+orders = db.Table('orders',
+    db.Column('buyer_id', db.Integer, db.ForeignKey('buyer.id'), primary_key=True),
+    db.Column('product_id', db.Integer, db.ForeignKey('product.id'), primary_key=True),
+    db.Column('order_date', db.DateTime, default=datetime.utcnow, nullable=False)
+)
 
-#     def __repr__(self):
-#         return '<Post %r>' % self.title
-
-#db.create_all()
+class Buyer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    orders = db.relationship('Product', secondary='orders', backref='buyers')
